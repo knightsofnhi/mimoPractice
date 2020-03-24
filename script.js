ddd7af1befa2fb3c55407f4da4ef161293171b0c
shuh <nonya@biz.com>
2020-03-25 00:03:14 +0800

    erase

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-class Comment {
-    constructor (name, comment) {
+// class BitcoinWallet {
+//     constructor(bitcoin) {
+//         this.bitcoin = bitcoin;
+//     }
+//     addBitcoins(bitcoin) {
+//         this.bitcoin = this.bitcoin + bitcoin;
+//         console.log(bitcoin + "bc add");
+//     }
+// }
+
+// var wallet = new itcoinWallet(0);
+//     wallet.addBitcoins(1);
+
+///////////////////////
+
+class User {
+    constructor(name) {
         this.name = name;
-        this.comment = comment;
+        this.isOnline = true;
+        this.status = "Hey, I'm using Mimo";
     }
 }
 
-var fan = new Fan("Margaret", "Knight");
+var user1 = new User("Arya");
+user1.sayHi();
+user1.isOnline = false;
+console.log(user1);
 
-console.log(fan);
+var user2 = new User("Bran");
+
+console.log(user2);
